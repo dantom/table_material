5d0d207297de5c04b6a47daf89e83d2b5b877321
dantom <dvtomescu@yahoo.com>
2021-03-03 18:21:24 +0200

    adaugare in repository

--- src/App.js
+++ src/App.js
@@ -1,25 +1,58 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableContainer from '@material-ui/core/TableContainer';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
+import Paper from '@material-ui/core/Paper';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const useStyles = makeStyles({
+  table: {
+    minWidth: 650,
+  },
+});
+
+
+const list=[{LastName:"Ionescu", FirstName:"Ion",Phone:"072121311"},{LastName:"Popescu",FirstName:"Vlad",Phone:"07622222"}]
+
+class App  extends React.Component {
+    constructor() {
+        super()
+        this.state = {
+            Persons:[...list]
+        }
+    }
+
+    render(){
+            //const classes = useStyles();
+
+            return (
+              <TableContainer component={Paper}>
+                <Table aria-label="simple table">
+                  <TableHead>
+                    <TableRow>
+                      <TableCell>Surname</TableCell>
+                      <TableCell >Name</TableCell>
+                      <TableCell align="right">Phone</TableCell>
+                    </TableRow>
+                  </TableHead>
+                  <TableBody>
+                    {this.state.Persons.map((person) => (
+                      <TableRow key={person.name}>
+                        <TableCell component="th" scope="row">
+                          {person.FirstName}
+                        </TableCell>
+                        <TableCell>{person.LastName}</TableCell>
+                        <TableCell align="right">{person.Phone}</TableCell>
+                      </TableRow>
+                    ))}
+                  </TableBody>
+                </Table>
+              </TableContainer>
+        )
+    }
 }
 
 export default App;
